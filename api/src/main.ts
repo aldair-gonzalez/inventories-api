b2a5af8db42bfa41977b7b6b64e691e1323902f7
Aldair González <aldairgome97@gmail.com>
2023-09-12 19:15:02 -0600

    Actualización del registro de la aplicación

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,16 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: new Logger() });
   app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
   const serverPort = configService.get<string>('SERVER_PORT');
   await app.listen(serverPort);
+  Logger.log(`Server running on port ${serverPort}`);
 }
 bootstrap();
