068e43c784566fa10c7f6d5b9ae564f1f9477355
Aldair González <aldairgome97@gmail.com>
2023-09-16 15:52:57 -0600

    Se corrigió el condicional dentro de la función parseTrim

--- api/src/utils/trim.ts
+++ api/src/utils/trim.ts
@@ -1,10 +1,11 @@
 export function ParseTrimFromDto(dto: any) {
   function generatedTrim(value: any): any {
-    if (typeof value === 'string') return value.trim() || value;
+    if (typeof value === 'string')
+      return value.trim().toUpperCase() || value.toUpperCase();
     else return value;
   }
   const keys = Object.keys(dto);
   Object.values(dto).map((value, i) => {
-    dto[keys[i]] = generatedTrim(value).toUpperCase();
+    dto[keys[i]] = generatedTrim(value);
   });
 }
