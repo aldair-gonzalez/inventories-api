41ff8b612280bb56dbb9947f1c74839476f0be7a
Aldair González <aldairgome97@gmail.com>
2023-09-23 15:16:58 -0600

    Actualizando la ruta de la documentación

--- api/src/main.ts
+++ api/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('/', app, document);
 
   const configService = app.get(ConfigService);
   const serverPort = configService.get<string>('SERVER_PORT');
