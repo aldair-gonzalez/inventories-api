20bf5f0e8e7c770f9812faf2a53cf6d4cf0abb3f
Aldair González <aldairgome97@gmail.com>
2023-09-23 15:14:18 -0600

    Implementando swagger para documentar la api

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,12 +3,22 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: new Logger() });
   app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+
+  // Config Swagger API
+  const config = new DocumentBuilder()
+    .setTitle('INVENTORIES API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const configService = app.get(ConfigService);
   const serverPort = configService.get<string>('SERVER_PORT');
   await app.listen(serverPort);
