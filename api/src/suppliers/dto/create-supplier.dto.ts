bdb53fc8931e381a765c47c2a4e853ad79c1d15d
Aldair González <aldairgome97@gmail.com>
2023-09-23 17:08:27 -0600

    swagger: Documentando la api de proveedores

--- api/src/suppliers/dto/create-supplier.dto.ts
+++ api/src/suppliers/dto/create-supplier.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsNotEmpty,
   IsNumber,
@@ -9,33 +10,42 @@ import {
 export class CreateSupplierDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   name: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   description: string;
 
   @IsString()
+  @ApiProperty()
   address: string;
 
   @IsString()
+  @ApiProperty()
   city: string;
 
   @IsString()
+  @ApiProperty()
   state: string;
 
   @IsOptional()
   @IsNumber()
+  @ApiProperty()
   zip_code: number;
 
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty()
   phone_number: number;
 
   @IsString()
+  @ApiProperty()
   email_address: string;
 
   @IsString()
+  @ApiProperty()
   website: string;
 
   @IsNotEmpty()
@@ -43,5 +53,6 @@ export class CreateSupplierDto {
     maxDecimalPlaces: 3,
   })
   @Min(0.001)
+  @ApiProperty()
   credit_limit: number;
 }
--- api/src/suppliers/suppliers.controller.ts
+++ api/src/suppliers/suppliers.controller.ts
@@ -14,11 +14,65 @@ import { SuppliersService } from './suppliers.service';
 import { CreateSupplierDto } from './dto/create-supplier.dto';
 import { UpdateSupplierDto } from './dto/update-supplier.dto';
 import { ParseTrimFromDto } from 'src/utils/trim';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('suppliers')
 @Controller('suppliers')
 export class SuppliersController {
   constructor(private readonly suppliersService: SuppliersService) {}
 
+  @ApiOperation({ summary: 'Create a new supplier' })
+  @ApiCreatedResponse({
+    description: 'The supplier has been successfully created',
+    schema: {
+      example: {
+        name: 'SUPPLIER',
+        description: 'SUPPLIER DESCRIPTION',
+        address: '',
+        city: '',
+        state: '',
+        zip_code: 11111,
+        phone_number: 1111111111,
+        email_address: '',
+        website: '',
+        credit_limit: 1000,
+        supplier_id: 1,
+      },
+    },
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad Request',
+    schema: {
+      example: {
+        message: [
+          'name must be a string',
+          'name should not be empty',
+          'description must be a string',
+          'description should not be empty',
+          'address must be a string',
+          'city must be a string',
+          'state must be a string',
+          'phone_number must be a number conforming to the specified constraints',
+          'phone_number should not be empty',
+          'email_address must be a string',
+          'website must be a string',
+          'credit_limit must not be less than 0.001',
+          'credit_limit must be a number conforming to the specified constraints',
+          'credit_limit should not be empty',
+        ],
+        error: 'Bad Request',
+        statusCode: 400,
+      },
+    },
+  })
   @Post()
   async create(@Body() createVendorDto: CreateSupplierDto) {
     try {
@@ -31,6 +85,27 @@ export class SuppliersController {
     }
   }
 
+  @ApiOperation({ summary: 'Get all suppliers' })
+  @ApiOkResponse({
+    description: 'All suppliers have been successfully retrieved',
+    schema: {
+      example: [
+        {
+          name: 'SUPPLIER',
+          description: 'SUPPLIER DESCRIPTION',
+          address: '',
+          city: '',
+          state: '',
+          zip_code: 11111,
+          phone_number: 1111111111,
+          email_address: '',
+          website: '',
+          credit_limit: 1000,
+          supplier_id: 1,
+        },
+      ],
+    },
+  })
   @Get()
   async findAll() {
     try {
@@ -42,6 +117,45 @@ export class SuppliersController {
     }
   }
 
+  @ApiOperation({ summary: 'Get a supplier' })
+  @ApiOkResponse({
+    description: 'The supplier has been successfully retrieved',
+    schema: {
+      example: {
+        name: 'SUPPLIER',
+        description: 'SUPPLIER DESCRIPTION',
+        address: '',
+        city: '',
+        state: '',
+        zip_code: 11111,
+        phone_number: 1111111111,
+        email_address: '',
+        website: '',
+        credit_limit: 1000,
+        supplier_id: 1,
+      },
+    },
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad Request',
+    schema: {
+      example: {
+        message: 'vendor_id should be a number',
+        error: 'Bad Request',
+        statusCode: 400,
+      },
+    },
+  })
+  @ApiNotFoundResponse({
+    description: 'Not found',
+    schema: {
+      example: {
+        message: 'Vendor not found',
+        error: 'Bad Request',
+        statusCode: 404,
+      },
+    },
+  })
   @Get(':vendor_id')
   async findOne(@Param('vendor_id') vendor_id: number) {
     try {
@@ -57,6 +171,38 @@ export class SuppliersController {
     }
   }
 
+  @ApiOperation({ summary: 'Update a supplier' })
+  @ApiOkResponse({
+    description: 'The supplier has been successfully updated',
+    schema: {
+      example: {
+        generatedMaps: [],
+        raw: [],
+        affected: 1,
+      },
+    },
+  })
+  @ApiNotFoundResponse({
+    description: 'Not found',
+    schema: {
+      example: {
+        message: 'Vendor not found',
+        error: 'Bad Request',
+        statusCode: 404,
+      },
+    },
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad Request',
+    schema: {
+      example: {
+        message:
+          'Cannot perform update query because update values are not defined. Call "qb.set(...)" method to specify updated values.',
+        error: 'Bad Request',
+        statusCode: 400,
+      },
+    },
+  })
   @Patch(':vendor_id')
   async update(
     @Param('vendor_id') vendor_id: number,
@@ -76,6 +222,20 @@ export class SuppliersController {
     }
   }
 
+  @ApiOperation({ summary: 'Delete a supplier' })
+  @ApiNoContentResponse({
+    description: 'The supplier has been successfully deleted',
+  })
+  @ApiNotFoundResponse({
+    description: 'Not found',
+    schema: {
+      example: {
+        message: 'Vendor not found',
+        error: 'Bad Request',
+        statusCode: 404,
+      },
+    },
+  })
   @Delete(':vendor_id')
   @HttpCode(204)
   async remove(@Param('vendor_id') vendor_id: number) {
