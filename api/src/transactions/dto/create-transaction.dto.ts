af18d44bdad8428b6b4ffe3536bd74615fb603e1
Aldair González <aldairgome97@gmail.com>
2023-09-24 18:53:01 -0600

    swagger: Añadiendo el api property al dto de transacciones

--- api/src/transactions/dto/create-transaction.dto.ts
+++ api/src/transactions/dto/create-transaction.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateTransactionDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   name: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   @IsString()
   description: string;
 }
